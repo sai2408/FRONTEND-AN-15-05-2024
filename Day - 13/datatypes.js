47b4752739bb1dcf2c9619f91b21989fbdc4ff63
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-05-30 16:43:38 +0530

    Add files via upload

---
+++ Day - 13/datatypes.js
@@ -0,0 +1,40 @@
+//Primitive Data Types
+var a = 100;
+var b = 20.56;
+var c = "abc123.56";
+var d = false;
+var e;
+var f = null;
+var g = Symbol(10);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+
+//Reference Data Types
+var x = {
+    name : "sai",
+    age : 30,
+    marks : 99.56
+}
+var y = [10,20,34.56,"sai",false];
+function z(){
+    console.log("I am Function");
+}
+console.log(x);
+console.log(y);
+z();
+
+//Special Data Types
+var m = NaN;
+var n1 = Infinity;
+var n2 = -Infinity;
+var o = 1234567890987654321234567890;
+console.log(m);
+console.log(n1);
+console.log(n2);
+console.log(o);
+
